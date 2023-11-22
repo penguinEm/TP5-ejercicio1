db68aed0c46515555cbbe92c084835aeda61f139
EmirParadi <emir.alejandro.paradi@gmail.com>
2023-11-22 09:37:22 -0300

    Funciona el boton para generar un numero entre 1 y 10

--- js/app.js
+++ js/app.js
@@ -2,12 +2,17 @@
 Cuando el usuario adivine el numero mostrar un mensaje indicando al usuario que adivino el numero.
  */
 
-
 //1er funion NUMERO ALEATORIO
 
-const numero_aleatorio() =>{
+const numero_aleatorio = () => {
+  const minimo = 1;
+  const maximo = 10;
+  const numero_obtenido = Math.floor(Math.random() * (maximo - minimo + 1) + minimo);
+  console.log(numero_obtenido)
+  return numero_obtenido
+};
 
-}
+const btn_comenzarJuego = document.querySelector("#btn_comenzar");
+btn_comenzarJuego.addEventListener("click", numero_aleatorio);
 
-const btn_comenzarJuego = document.querySelector("#btn_comenzar")
-btn_comenzarJuego.addEventListener("click", numero_aleatorio())
+const input_numero_aleatorio = document.querySelector("form-control")
